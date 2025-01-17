c71959b35982ea75dc52e5f7664cf25b6ea25d70
Joseph Cryer <jcryer1234@gmail.com>
2025-01-17 13:24:47 +0000

    remove server timeout value

--- src/models/Config.java
+++ src/models/Config.java
@@ -4,14 +4,12 @@ public class Config {
   public Integer terminalPort;
   public Integer terminalTimeout;
   public Integer serverPort;
-  public Integer serverTimeout;
   public Integer tid;
-  Config(String terminalIp, Integer terminalPort, Integer terminalTimeout, Integer serverPort, Integer serverTimeout, Integer tid) {
+  Config(String terminalIp, Integer terminalPort, Integer terminalTimeout, Integer serverPort, Integer tid) {
     this.terminalIp = terminalIp;
     this.terminalPort = terminalPort;
     this.terminalTimeout = terminalTimeout;
     this.serverPort = serverPort;
-    this.serverTimeout = serverTimeout;
     this.tid = tid;
   }
 }
